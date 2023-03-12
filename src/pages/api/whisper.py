c6654519d5bf6c40d291f6c48b15d338a9a15ba8
MikeFreno <michaelt.freno@gmail.com>
2023-03-12 10:15:30 -0400

    uncommnet

--- src/pages/api/whisper.py
+++ src/pages/api/whisper.py
@@ -9,12 +9,11 @@ app = Sanic()
 
 @app.post("/api/whisper")
 async def index(request):
-    # file_content = request.body.decode()
-    # model = whisper.load_model("base")
-    # result = model.transcribe(
-    #     f"/src/uploads/{file_content}", fp16=False, language="English"
-    # )
-    # wrapped_text = textwrap.fill(result["text"], width=80, break_long_words=False)
-    # os.remove(f"/src/uploads/{file_content}")
-    # return json({wrapped_text})
-    return text("HELLO")
+    file_content = request.body.decode()
+    model = whisper.load_model("base")
+    result = model.transcribe(
+        f"/src/uploads/{file_content}", fp16=False, language="English"
+    )
+    wrapped_text = textwrap.fill(result["text"], width=80, break_long_words=False)
+    os.remove(f"/src/uploads/{file_content}")
+    return json({wrapped_text})
